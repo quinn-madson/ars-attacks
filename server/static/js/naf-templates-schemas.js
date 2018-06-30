38b17e311ba6521dd65381c81dd7a0b3aa545b79
Quinn Madson <quinn@3data.io>
2018-06-30 17:37:28 -0500

    Added html-imports polyfill for firefox and edge.

--- server/static/js/naf-templates-schemas.js
+++ server/static/js/naf-templates-schemas.js
@@ -1,19 +1,29 @@
 // Import templates
 var link = document.querySelector('link[rel="import"]');
-var template = link.import.querySelector('template');
-var clone = document.importNode(template, true);
-document.querySelector('a-assets').appendChild(clone);
+if (link.import) {
+  doImports();
+} else {
+  window.addEventListener('HTMLImportsLoaded', function (e) {
+    doImports();
+  });
+}
 
-// Define custom schema for syncing avatar color, set by random-color
-NAF.schemas.add({
-  template: '#avatar-template',
-  components: [
-    'position',
-    'rotation',
-    {
-      selector: '.head',
-      component: 'material',
-      property: 'color'
-    }
-  ]
-});
+function doImports() {
+  var template = link.import.querySelector('template');
+  var clone = document.importNode(template, true);
+  document.querySelector('a-assets').appendChild(clone);
+
+  // Define custom schema for syncing avatar color, set by random-color
+  NAF.schemas.add({
+    template: '#avatar-template',
+    components: [
+      'position',
+      'rotation',
+      {
+        selector: '.head',
+        component: 'material',
+        property: 'color'
+      }
+    ]
+  });
+}

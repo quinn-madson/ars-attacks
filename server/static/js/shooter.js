bad1c246a3ffccd8fdbe252d87a6b67ff46d6bde
Will Murphy <william@datavizvr.com>
2018-06-30 17:37:25 -0500

    vr controller support

--- server/static/js/shooter.js
+++ server/static/js/shooter.js
@@ -1,10 +1,18 @@
 window.AFRAME.registerComponent('shooter', {
+  schema: {
+    direction: { type: 'vec3', default: { x: 0, y: 0, z: -1 } }
+  },
   init: function () {
     this.shoot = window.AFRAME.utils.throttle(this.shoot, 300, this)
     document.addEventListener('mousedown', this.shoot)
     document.addEventListener('touchstart', this.shoot)
     this.el.addEventListener('triggerdown', this.shoot)
     this.el.addEventListener('trackpaddown', this.shoot)
+    this.el.addEventListener('controllerconnected', evt => {
+      if (evt.detail.name === 'oculus-touch-controls') {
+        this.el.setAttribute('shooter', { direction: { x: 0, y: -0.8, z: -1 } })
+      }
+    })
   },
   shoot: (function () {
     const ray = new window.THREE.Ray()
@@ -12,7 +20,7 @@ window.AFRAME.registerComponent('shooter', {
     return function () {
       const proj = document.createElement('a-entity')
       const rotation = this.el.getAttribute('rotation')
-      direction.set(0, 0, -1)
+      direction.copy(this.data.direction)
       direction.transformDirection(this.el.object3D.matrixWorld).normalize()
       ray.set(this.el.object3D.position, direction)
       proj.setAttribute('position', ray.at(0.1))
